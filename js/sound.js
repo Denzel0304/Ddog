71864f3b1e2e7950da6f087a527a814c638a7d07
Denzel0304 <maru3choon@gmail.com>
2026-04-15 10:32:00 +0900

    Add files via upload

--- js/sound.js
+++ js/sound.js
@@ -7,7 +7,7 @@ let _completeSoundAudio = null;
 function playCompleteSound() {
   try {
     if (!_completeSoundAudio) {
-      _completeSoundAudio = new Audio('effect 1.mp3');
+      _completeSoundAudio = new Audio('effect%201.mp3');
       _completeSoundAudio.volume = 0.7;
     }
     _completeSoundAudio.currentTime = 0;
