cdadef5f95363a9520a8429dd98c359653425e4f
Denzel0304 <maru3choon@gmail.com>
2026-06-02 20:25:27 +0900

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -73,11 +73,12 @@ function initBackButton() {
 
   window.addEventListener('popstate', e => {
     if (e.state && e.state.page === 'base') {
-      history.pushState({ page: 'app' }, '');
       if (hasOpenPopup()) {
         closeTopPopup();
+        history.pushState({ page: 'app' }, '');
       } else if (currentTab !== 'todo') {
         switchTab('todo');
+        history.pushState({ page: 'app' }, '');
       }
     }
   });
