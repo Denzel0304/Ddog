f02439d4dcfd2d9a84191317ce16e4092930e59e
Denzel0304 <maru3choon@gmail.com>
2026-08-12 09:04:59 +0900

    Add files via upload

--- js/modal.js
+++ js/modal.js
@@ -134,7 +134,9 @@ function openEditModal(todo) {
   document.getElementById('modal-title-label').textContent = '할일 수정';
   resetModalForm();
   document.getElementById('input-title').value  = todo.title || '';
-  document.getElementById('input-memo').value   = todo.memo  || '';
+  const memoEl = document.getElementById('input-memo');
+  memoEl.value = todo.memo || '';
+  memoEl.scrollTop = 0;
   document.getElementById('input-date').value   = todo.date  || todayStr();
   document.getElementById('input-remind').value = todo.remind_days || 0;
   document.getElementById('input-weekly-flag').checked = !!todo.weekly_flag;
